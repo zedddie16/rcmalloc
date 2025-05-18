cdc7184d6f4fccd118e0b721579704c7856360b2
zedddie16 <zedddiezxc@gmail.com>
2025-05-18 22:59:26 +0200

    added base structs of allocator

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,17 @@ const ARENA_SIZE: usize = 128 * 1024;
 // Any allocation requiring alignment up to this value will be placed at an address
 // divisible by it. It does not define a max allocation size.
 const MAX_SUPPORTED_ALIGN: usize = 4096;
+
+#[repr(C, align(4096))] // MAX_SUPPORTED_ALIGN
+struct ReallyCoolAllocator {
+    arena: UnsafeCell<[u8; ARENA_SIZE]>,
+    remaining: AtomicUsize,
+}
+
+// #[global_allocator]     when basic required methods will be implemented
+static ALLOCATOR: ReallyCoolAllocator = ReallyCoolAllocator {
+    arena: UnsafeCell::new([0x55; ARENA_SIZE]),
+    remaining: AtomicUsize::new(ARENA_SIZE),
+};
+
+unsafe impl Sync for ReallyCoolAllocator {}
