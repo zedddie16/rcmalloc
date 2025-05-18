90546277b4a16c7370e2db6d3607b77e34ed2d82
zedddie16 <zedddiezxc@gmail.com>
2025-05-18 22:14:07 +0200

    comments for my own understanding

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,10 @@ use std::cell::UnsafeCell;
 use std::ptr::null_mut;
 use std::sync::atomic::{AtomicUsize, Ordering::Relaxed};
 
+// ARENA_SIZE corresponds to HEAP size. n * 1024 where n is the quantity of bytes
 const ARENA_SIZE: usize = 128 * 1024;
+
+// MAX_SUPPORTED_ALIGN is the largest alignment allocator guarantees to support.
+// Any allocation requiring alignment up to this value will be placed at an address
+// divisible by it. It does not define a max allocation size.
 const MAX_SUPPORTED_ALIGN: usize = 4096;
