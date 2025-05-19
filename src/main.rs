a88f78a6b481a5fe56ccffb333718ede68054fa8
zedddie16 <zedddiezxc@gmail.com>
2025-05-19 06:39:00 +0200

    some funny benchmarks bc me and my friend were curious

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
+use std::time::Instant;
 // if not explicitly used, uses default allocator
 #[allow(unused_imports)]
 use rcmalloc::*;
 
 fn main() {
-    let v = Box::new(123_u64);
-    println!("Allocated value: {}", v);
-
-    let mut vec = Vec::new();
     let mut counter = 0;
-    loop {
-        let v = 1_u64;
-        vec.push(v);
-
+    let timer = Instant::now();
+    while counter < 1310590_u64 {
+        let _v = 1_u64;
         counter += 1;
         println!("counter: {}", counter);
     }
+    let elapsed = timer.elapsed();
 
-    println!("counter: {}", counter);
+    let timer2 = Instant::now();
+
+    counter = 0;
+    while counter < 1310590_u64 {
+        let _v = Box::new(1_u64);
+        counter += 1;
+        println!("counter: {}", counter);
+    }
+    let elapsed2 = timer2.elapsed();
+
+    println!("time spent by STACK allocations ==> {:?}", elapsed);
+    println!("time spent by HEAP allocations ==> {:?}", elapsed2);
 }
