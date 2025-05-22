406adb99a6ecb463d9bd70118b13786a3d3bbece
zedddie16 <zedddiezxc@gmail.com>
2025-05-22 02:36:12 +0200

    rm redundant unsafe blocks

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::alloc::{GlobalAlloc, Layout};
-use std::mem::MaybeUninit;
 use std::cell::UnsafeCell;
+use std::mem::MaybeUninit;
 use std::ptr::NonNull;
 use std::ptr::null_mut;
 use std::sync::atomic::{AtomicUsize, Ordering::Relaxed};
@@ -28,24 +28,23 @@ pub struct MemoryList<'b> {
     next: Option<&'b mut MemoryList<'b>>,
 }
 
-
 impl ReallyCoolAllocator<'static> {
-
-unsafe fn alloc_metadata_node<'a>(&'a self) -> Option<&'a mut MemoryList<'a>> {
+    unsafe fn alloc_metadata_node<'a>(&'a self) -> Option<&'a mut MemoryList<'a>> {
         unsafe {
-    let offset = self.meta_offset.get();
-    let node_size = std::mem::size_of::<MemoryList>();
-    let align = std::mem::align_of::<MemoryList>();
+            let offset = self.meta_offset.get();
+            let node_size = std::mem::size_of::<MemoryList>();
+            let align = std::mem::align_of::<MemoryList>();
 
-    let align_offset = ((*offset).load(Relaxed) + align - 1) & !(align - 1);
-    if align_offset + node_size > ARENA_SIZE {
-        return None;
+            let align_offset = ((*offset).load(Relaxed) + align - 1) & !(align - 1);
+            if align_offset + node_size > ARENA_SIZE {
+                return None;
+            }
+
+            let ptr = self.arena.get().cast::<u8>().add(align_offset) as *mut MemoryList<'a>;
+            *self.meta_offset.get() = AtomicUsize::from(align_offset + node_size);
+            Some(&mut *ptr)
+        }
     }
-
-    let ptr = self.arena.get().cast::<u8>().add(align_offset) as *mut MemoryList<'a>;
-    *self.meta_offset.get() = AtomicUsize::from( align_offset + node_size) ;
-    Some(&mut *ptr)
-}}
 }
 #[global_allocator]
 pub static ALLOCATOR: ReallyCoolAllocator = ReallyCoolAllocator {
@@ -64,97 +63,88 @@ pub static ALLOCATOR: ReallyCoolAllocator = ReallyCoolAllocator {
 unsafe impl Sync for ReallyCoolAllocator<'static> {}
 
 unsafe impl GlobalAlloc for ReallyCoolAllocator<'static> {
-
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         unsafe {
-        let size = layout.size();
-        let align = layout.align();
+            let size = layout.size();
+            let align = layout.align();
 
-
-let mut head_initialized = false;
-        if (*self.meta_offset.get()).load(Relaxed) == 0 { 
-            match self.alloc_metadata_node() {
-                Some(head_node) => {
-                    let ptr_to_head_uninit = self.head.get();
-                    (*ptr_to_head_uninit).write(MemoryList {
-                        ptr: NonNull::dangling(), 
-                        layout: Layout::from_size_align_unchecked(0, 1),
-                        free: true, 
-                        next: None,
-                    });
-                    head_initialized = true;
-                }
-                None => {
-                    if cfg!(feature = "debug_alloc") {
-                        eprintln!("rcmalloc: Failed to allocate initial head metadata node.");
+            let mut head_initialized = false;
+            if (*self.meta_offset.get()).load(Relaxed) == 0 {
+                match self.alloc_metadata_node() {
+                    Some(head_node) => {
+                        let ptr_to_head_uninit = self.head.get();
+                        (*ptr_to_head_uninit).write(MemoryList {
+                            ptr: NonNull::dangling(),
+                            layout: Layout::from_size_align_unchecked(0, 1),
+                            free: true,
+                            next: None,
+                        });
+                        head_initialized = true;
                     }
-                    return null_mut();
+                    None => {
+                        if cfg!(feature = "debug_alloc") {
+                            eprintln!("rcmalloc: Failed to allocate initial head metadata node.");
+                        }
+                        return null_mut();
+                    }
                 }
             }
-        }
 
-        if align > MAX_SUPPORTED_ALIGN {
+            if align > MAX_SUPPORTED_ALIGN {
+                if cfg!(feature = "debug_alloc") {
+                    eprintln!(
+                        "rcmalloc: Requested alignment {} exceeds MAX_SUPPORTED_ALIGN",
+                        align
+                    );
+                }
+                return null_mut();
+            }
+
+            let current_remaining = self.remaining.load(Relaxed);
+            let current_offset = ARENA_SIZE - current_remaining;
+            let aligned_offset = (current_offset + (align - 1)) & !(align - 1);
+            let required_space = aligned_offset + size;
+
             if cfg!(feature = "debug_alloc") {
                 eprintln!(
-                    "rcmalloc: Requested alignment {} exceeds MAX_SUPPORTED_ALIGN",
-                    align
+                    "rcmalloc: alloc(size={}, align={}) - current_remaining={}, current_offset={}, aligned_offset={}, required_space={}",
+                    size, align, current_remaining, current_offset, aligned_offset, required_space
                 );
             }
-            return null_mut();
-        }
 
-        let current_remaining = self.remaining.load(Relaxed);
-        let current_offset = ARENA_SIZE - current_remaining;
-        let aligned_offset = (current_offset + (align - 1)) & !(align - 1);
-        let required_space = aligned_offset + size;
-
-        if cfg!(feature = "debug_alloc") {
-            eprintln!(
-                "rcmalloc: alloc(size={}, align={}) - current_remaining={}, current_offset={}, aligned_offset={}, required_space={}",
-                size, align, current_remaining, current_offset, aligned_offset, required_space
-            );
-        }
-
-        if required_space > ARENA_SIZE {
-            if cfg!(feature = "debug_alloc") {
-                eprintln!("rcmalloc: Out of memory - required_space > ARENA_SIZE");
+            if required_space > ARENA_SIZE {
+                if cfg!(feature = "debug_alloc") {
+                    eprintln!("rcmalloc: Out of memory - required_space > ARENA_SIZE");
+                }
+                return null_mut();
             }
-            return null_mut();
-        }
 
-        let allocation_size = size;
-        let new_remaining = self
-            .remaining
-            .fetch_sub(allocation_size + (aligned_offset - current_offset), Relaxed);
+            let allocation_size = size;
+            let new_remaining = self
+                .remaining
+                .fetch_sub(allocation_size + (aligned_offset - current_offset), Relaxed);
 
-        if cfg!(feature = "debug_alloc") {
-            eprintln!(
-                "rcmalloc: new_remaining after fetch_sub = {}",
-                new_remaining
-            );
-        }
-
-        if new_remaining < allocation_size + (aligned_offset - current_offset) {
             if cfg!(feature = "debug_alloc") {
                 eprintln!(
-                    "rcmalloc: Allocation failed due to race or insufficient space after subtraction"
+                    "rcmalloc: new_remaining after fetch_sub = {}",
+                    new_remaining
                 );
             }
-            self.remaining
-                .fetch_add(allocation_size + (aligned_offset - current_offset), Relaxed);
-            return null_mut();
-        }
 
-        let ptr = unsafe { self.arena.get().cast::<u8>().add(aligned_offset) };
+            if new_remaining < allocation_size + (aligned_offset - current_offset) {
+                if cfg!(feature = "debug_alloc") {
+                    eprintln!(
+                        "rcmalloc: Allocation failed due to race or insufficient space after subtraction"
+                    );
+                }
+                self.remaining
+                    .fetch_add(allocation_size + (aligned_offset - current_offset), Relaxed);
+                return null_mut();
+            }
 
+            let ptr = self.arena.get().cast::<u8>().add(aligned_offset);
 
-        self.head.layout = layout;
-        self.head.ptr = unsafe { NonNull::new_unchecked(ptr) };
-        self.head.free = false;
-        self.head.next = ;
-
-        if cfg!(feature = "debug_alloc") {
-            unsafe {
+            if cfg!(feature = "debug_alloc") {
                 static mut COUNT: usize = 0;
                 COUNT += 1;
                 let current_count = COUNT;
@@ -163,9 +153,9 @@ let mut head_initialized = false;
                     ptr, current_count
                 );
             }
-        }
 
-        ptr // alloc must return **hopefully** valid pointer to where data block starts
-    }}
+            ptr // alloc must return **hopefully** valid pointer to where data block starts
+        }
+    }
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {}
 }
